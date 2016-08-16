1887b948b46cd624b6b4cf100b3e30c1c30b9b23
LightbulbProductions <gerbermichelle23@gmail.com>
2016-08-16 12:48:45 +0000

    moved gems for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160816002055) do
+ActiveRecord::Schema.define(version: 20160816045508) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20160816002055) do
     t.decimal  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image"
   end
 
 end
